e4375992cf67d39322ef8f233706edecfbc10550
Svetlana <kuznetsova@66.ru>
2021-10-01 00:38:06 +0500

    bugs fixed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 function getRandomNumber(min, max) {
   const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-  const messageError = 'Диапазон задан неверно!';
   if (min >= 0 && max > 0 && min < max) {
     return randomNumber;
   }
-  return messageError;
+  throw new Error('Диапазон задан неверно!');
 }
 getRandomNumber(100, 200);
 
 function checkStringLength(string, maxLength) {
-  const messageError = 'Превышение допустимого числа символов';
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return messageError;
+  return string.length <= maxLength;
 }
 checkStringLength('Проверяемая строка', 40);
