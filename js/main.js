fe0f0c64a34886e557b66aea1956979492f8cbff
Svetlana <kuznetsova@66.ru>
2021-10-08 23:14:06 +0500

    some fixes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 /* eslint-disable id-length */
 
-function getRandomPositiveInteger (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+function getRandomPositiveInteger (from, to) {
+  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
+  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
-
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
-checkStringLength('Проверяемая строка', 40);
 
 const DESCRIPTION = [
   'Осенняя пора',
