dab535207278c6809c21e4072a6e2589e1d2e7c1
Svetlana <kuznetsova@66.ru>
2021-11-15 02:05:30 +0500

    some fixes 8.11

--- js/form.js
+++ js/form.js
@@ -10,57 +10,68 @@ const body = document.querySelector('body');
 const closeButton = document.querySelector('#upload-cancel');
 const hashtagInput = document.querySelector('.text__hashtags');
 const regex = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
-
 const textDescriptionInput = document.querySelector('.text__description');
+
+const validateHashtags = () => {
+  hashtagInput.addEventListener('input', () => {
+    const hashtagsArray = hashtagInput.value.split('');
+    if (hashtagsArray[0] !== '#') {
+      hashtagInput.setCustomValidity('Хэштег должен начинаться с #');
+    } else if (hashtagsArray.length > MAX_HASHTAG_COUNT) {
+      hashtagInput.setCustomValidity('Нельзя указать больше 5 хэштегов');
+    } else {
+      hashtagsArray.forEach((hashtag) => {
+        if (hashtag === '#') {
+          hashtagInput.setCustomValidity('Хэштег не может состоять только из одной решётки');
+        } else if (hashtag.length > MAX_HASHTAG_LENGTH) {
+          hashtagInput.setCustomValidity('Максимальная длина хэштега 20 символов');
+        } else if (!regex.test(hashtag)) {
+          hashtagInput.setCustomValidity('Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи');
+        } else {
+          hashtagInput.setCustomValidity('');
+        }
+      });
+    }
+    hashtagInput.reportValidity();
+  });
+};
+
+const validateTextDescription = () => {
+  textDescriptionInput.addEventListener('input', () => {
+    if (textDescriptionInput.value.length > MAX_COMMENT_LENGTH) {
+      textDescriptionInput.setCustomValidity('Длина коммментария не может превышать 140 символов');
+    } else {
+      textDescriptionInput.setCustomValidity('');
+    }
+    textDescriptionInput.reportValidity();
+  });
+};
+
+const onUploadEscKeydown = (evt) => {
+  if(isEscapeKey(evt)) {
+    evt.preventDefault();
+    overlay.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
+};
 
 const closeForm = () => {
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
+  hashtagInput.removeEventListener('input', validateHashtags);
+  textDescriptionInput.removeEventListener('input', validateTextDescription);
   document.removeEventListener('keydown', onUploadEscKeydown);
 };
-
-function onUploadEscKeydown (evt) {
-  if(isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeForm();
-  }
-}
 
 const openForm = () => {
   overlay.classList.remove('hidden');
   body.classList.add('modal-open');
+  hashtagInput.addEventListener('input', validateHashtags);
+  textDescriptionInput.addEventListener('input', validateTextDescription);
   document.addEventListener('keydown', onUploadEscKeydown);
 };
 
 uploadFile.addEventListener('change', openForm);
 closeButton.addEventListener('click', closeForm);
 
-hashtagInput.addEventListener('input', () => {
-  const hashtagsArray = hashtagInput.value.split('');
-
-  hashtagsArray.forEach((hashtag) => {
-    if (hashtagsArray[0] !== '#') {
-      hashtagInput.setCustomValidity('Хэштег должен начинаться с #');
-    } else if (hashtag === '#') {
-      hashtagInput.setCustomValidity('Хэштег не может состоять только из одной решётки');
-    } else if (hashtag.length > MAX_HASHTAG_LENGTH) {
-      hashtagInput.setCustomValidity('Максимальная длина хэштега 20 символов');
-    } else if (hashtagsArray.length > MAX_HASHTAG_COUNT) {
-      hashtagInput.setCustomValidity('Нельзя указать больше 5 хэштегов');
-    } else if (!regex.test(hashtag)) {
-      hashtagInput.setCustomValidity('Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи');
-    } else {
-      hashtagInput.setCustomValidity('');
-    }
-    hashtagInput.reportValidity();
-  });
-});
-
-textDescriptionInput.addEventListener('input', () => {
-  if (textDescriptionInput.value.length > MAX_COMMENT_LENGTH) {
-    textDescriptionInput.setCustomValidity('Длина коммментария не может превышать 140 символов');
-  } else {
-    textDescriptionInput.setCustomValidity('');
-  }
-  textDescriptionInput.reportValidity();
-});
+export {closeForm, openForm};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import {generateObjects, PHOTO_DESCRIPTION_COUNT} from './data.js';
 import {createThumbnails} from './thumbnails.js';
+import './form.js';
 
 generateObjects(PHOTO_DESCRIPTION_COUNT);
 
 const generateThumbnails = generateObjects(PHOTO_DESCRIPTION_COUNT);
 createThumbnails(generateThumbnails);
+
